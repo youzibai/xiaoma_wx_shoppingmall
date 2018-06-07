c0608dad5aa48d45d34da2b7ddce036861ae538c
youzibai <youzibai@sohu.com>
2018-06-07 11:23:11 +0800

    Revert "1"
    This reverts commit 93ba8148bd949f17d7d0bdbd70447c546fe38297.

--- files/绑定手机号/data.js
+++
@@ -1,7 +0,0 @@
-﻿$axure.loadCurrentPage(
-(function() {
-    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
-    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,_(j,k),l,[m],n,_(o,p,q,r,s,t,u,_(),v,_(w,x,y,z,A,_(B,C,D,E),F,null,G,z,H,z,I,J,K,null,L,M,N,O,P,Q,R,M),S,_(),T,_(),U,_(V,[_(W,X,Y,j,Z,ba,q,bb,bc,bb,bd,be,v,_(w,bf,bg,_(bh,bi,bj,bk)),S,_(),bl,_(),bm,_(bn,bo))])),bp,_(),bq,_(br,_(bs,bt)));}; 
-var b="url",c="绑定手机号.html",d="generationDate",e=new Date(1528341559819.99),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="sketchKeys",j="",k="s0",l="variables",m="OnLoadVariable",n="page",o="packageId",p="2fb8680da3e84dc8a0c7d395afbf1bde",q="type",r="Axure:Page",s="name",t="绑定手机号",u="notes",v="style",w="baseStyle",x="627587b6038d43cca051c114ac41ad32",y="pageAlignment",z="near",A="fill",B="fillType",C="solid",D="color",E=0xFFFFFFFF,F="image",G="imageHorizontalAlignment",H="imageVerticalAlignment",I="imageRepeat",J="auto",K="favicon",L="sketchFactor",M="0",N="colorStyle",O="appliedColor",P="fontName",Q="Applied Font",R="borderWidth",S="adaptiveStyles",T="interactionMap",U="diagram",V="objects",W="id",X="2e9186b59890465183acd1b3ae9f7555",Y="label",Z="friendlyType",ba="Image",bb="imageBox",bc="styleType",bd="visible",be=true,bf="75a91ee5b9d042cfa01b8d565fe289c0",bg="size",bh="width",bi=375,bj="height",bk=647,bl="imageOverrides",bm="images",bn="normal~",bo="images/绑定手机号/u22.png",bp="masters",bq="objectPaths",br="2e9186b59890465183acd1b3ae9f7555",bs="scriptId",bt="u22";
-return _creator();
-})());
